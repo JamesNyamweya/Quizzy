cdec3c8f0e67cf201bb145cdcffbf7aaaad53e27
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-04-25 13:02:43 +0300

    Changed QuizZone to Quizzy

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({ username }) {
   return (
     <nav className="bg-blue-800 text-white p-4 flex justify-between items-center">
       <Link to="/" className="text-xl font-bold">
-        QuizZone
+        Quizzy
       </Link>
       {username && (
         <div className="text-sm">
