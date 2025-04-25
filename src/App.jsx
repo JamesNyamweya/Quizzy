ae7d0521616f54c2cfb08e5e4d11e2f5b4cd78b4
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-04-25 08:54:29 +0300

    Added Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Quizpage from "./Components/Quizpage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
+
 
 function App() {
   return (
     <>
       <Quizpage />
+      <ToastContainer />
     </>
   );
 }
